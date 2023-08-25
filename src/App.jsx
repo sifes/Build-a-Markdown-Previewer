67671ed420af2df8a415c049f6f1bcd9f8397bb7
sifes <lonfark@gmail.com>
2023-08-25 08:59:24 +0300

    cats + refact

--- src/App.jsx
+++ src/App.jsx
@@ -2,74 +2,71 @@ import { useState } from 'react';
 import { marked } from 'marked';
 import './App.css';
 marked.setOptions({
-  breaks:true
+  breaks: true
 })
 const renderer = new marked.Renderer()
 function App() {
-	const codeBlock =
-		'`' +
-		'`' +
-		'`' +
-		`
+  const codeBlock =
+    '`' +
+    '`' +
+    '`' +
+    `
   function anotherExample(firstLine, lastLine) {
     if (firstLine == 'a' && lastLine == 'a') {
       return multiLineCode;
     }
   }
   ` +
-		'`' +
-		'`' +
-		'`' +
-		`
+    '`' +
+    '`' +
+    '`' +
+    `
   `;
-	const list = `
+  const list = `
   - And of course there are lists.
   - Some are bulleted.
   - With different indentation levels.
   - That look like this.
   `;
-	const heading = `
+  const heading = `
   # h1 Heading
   `;
-	const heading2 = `
+  const heading2 = `
   ## h2 Heading
   `;
-	const link = `
+  const link = `
   There's also [links](https://www.freecodecamp.org), and
   > Block Quotes!
   `;
-	const code = 'Heres some code,  `<div></div>,` between 2 backticks.';
-	const bold = `
+  const code = 'Heres some code,  `<div></div>,` between 2 backticks.';
+  const bold = `
   You can also make text **bold**... whoa!
   `;
-	const image = `
-  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-  `;
-	const initialValue = [heading, heading2, link, codeBlock, code, list, bold, image];
-	console.log(list);
-	const [textareaValue, setTextareaValue] = useState(initialValue.join(' '));
-	return (
-		<>
-			<div className='container'>
-				<textarea name='editor' className='editor' id='editor' cols={30} rows={10} onChange={e => setTextareaValue(e.target.value)} value={textareaValue}></textarea>
-				<div className='preview' >
+  const image = `\n![freeCodeCamp Logo](https://thumbs.dreamstime.com/b/four-cute-cats-20650677.jpg)\n`;
+  const initialValue = [heading, heading2, link, codeBlock, code, list, bold, image];
+  console.log(list);
+  const [textareaValue, setTextareaValue] = useState(initialValue.join(' '));
+  return (
+    <>
+      <div className='container'>
+        <textarea name='editor' className='editor' id='editor' cols={30} rows={10} onChange={e => setTextareaValue(e.target.value)} value={textareaValue}></textarea>
+        <div className='preview' >
           <Preview markdown={textareaValue} />
-				</div>
-			</div>
-		</>
-	);
+        </div>
+      </div>
+    </>
+  );
 }
 
-function Preview({markdown}) {
+function Preview({ markdown }) {
   return (
     <div id='preview'
-    dangerouslySetInnerHTML={{
-      __html: marked(markdown, {renderer: renderer})
-    }}
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown, { renderer: renderer })
+      }}
     >
-
     </div>
   )
-  
+
 }
 export default App;
